91a94917c1552a88902ba8601ff06b790032245c
younglym <groupware@hanmail.net>
2024-06-26 14:46:09 +0900

    5강컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -2,23 +2,52 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
   'name': '이순신',
   'birthday': '961226',
   'gender': '남자',
   'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday': '971226',
+  'gender': '남자',
+  'job': '대학생2'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '강감찬',
+  'birthday': '981226',
+  'gender': '여자',
+  'job': '대학생3'
 }
-
+]
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}      
-      />
-    );
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer 
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+    );    
   }
 }
 
