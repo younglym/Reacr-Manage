f870c411005604e6f3ade11ccaa78ff8e4a2e7d5
younglym <groupware@hanmail.net>
2024-06-26 13:37:12 +0900

    4강 매개값 전달

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
+const customer = {
+  'name': '이순신',
+  'birthday': '961226',
+  'gender': '남자',
+  'job': '대학생'
+}
+
 class App extends Component {
   render() {
     return (
-      <Customer/>
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}      
+      />
     );
   }
 }
