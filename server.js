c6422a9956d8c1ec4d625e12b250d60f928639a0
younglym <groupware@hanmail.net>
2024-06-27 17:51:34 +0900

    11강 마리아DB 연결하기

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +8,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mariadb = require('mysql');
+
+const connection = mariadb.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-          {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '961222',
-            'gender': '남자',
-            'job': '대학생'
-          },        
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '이순신',
-            'birthday': '971222',
-            'gender': '남자',
-            'job': '대학생2'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '강감찬',
-            'birthday': '981222',
-            'gender': '남자',
-            'job': '대학생3'
-          }
-    ]);
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+   }
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
