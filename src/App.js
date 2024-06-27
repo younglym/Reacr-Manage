1a88ddbf3164336e2f7bcd22cc76e2b164834874
younglym <groupware@hanmail.net>
2024-06-27 09:02:24 +0900

    6강

--- src/App.js
+++ src/App.js
@@ -1,54 +1,82 @@
 import React, { Component } from 'react';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
 import './App.css';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import { withStyles } from '@mui/material/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '이순신',
-  'birthday': '961226',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '홍길동',
-  'birthday': '971226',
-  'gender': '남자',
-  'job': '대학생2'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '강감찬',
-  'birthday': '981226',
-  'gender': '여자',
-  'job': '대학생3'
-}
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '이순신',
+    'birthday': '971222',
+    'gender': '남자',
+    'job': '대학생2'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '강감찬',
+    'birthday': '981222',
+    'gender': '남자',
+    'job': '대학생3'
+  }
 ]
 class App extends Component {
   render() {
+    const { classes} = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Customer 
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            );
-          })
-        }
-      </div>
-    );    
+      //<Paper className={classes.root}>
+      //<div>
+        <Table >
+        <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>  
+
+          <TableBody>
+              {
+                customers.map(c => {
+                return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>  ); })
+              }
+          </TableBody>
+        </Table>
+     //</div>   
+     //</Paper>
+    );
   }
 }
 
 export default App;
+//export default withStyles(styles)(App);
